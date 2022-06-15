a28c0e3757a5e344514e088a5dc3c3abb4052c7f
Nemanja Delic <delneman@gmail.com>
2022-06-15 09:11:01 +0000

    Initial commit

---
+++ Puppetfile
@@ -0,0 +1,17 @@
+# This Puppetfile is managed by Bolt. Do not edit.
+# For more information, see https://pup.pt/bolt-modules
+
+# The following directive installs modules to the managed moduledir.
+moduledir '.modules'
+
+mod 'puppetlabs/ntp', '9.1.1'
+mod 'puppetlabs/apt', '8.4.0'
+mod 'puppetlabs/postgresql', '7.5.0'
+mod 'puppetlabs/puppetdb', '7.10.0'
+mod 'theforeman/puppet', '16.2.0'
+mod 'puppetlabs/stdlib', '8.2.0'
+mod 'puppetlabs/concat', '7.2.0'
+mod 'puppetlabs/inifile', '5.3.0'
+mod 'puppetlabs/firewall', '3.5.0'
+mod 'puppet/extlib', '6.0.0'
+mod 'puppet/systemd', '3.9.0'
---
+++ plans/init.pp
@@ -0,0 +1,33 @@
+# This is the structure of a simple plan. To learn more about writing
+# Puppet plans, see the documentation: http://pup.pt/bolt-puppet-plans
+
+# The summary sets the description of the plan that will appear
+# in 'bolt plan show' output. Bolt uses puppet-strings to parse the
+# summary and parameters from the plan.
+# @summary A plan created with bolt plan new.
+# @param targets The targets to run on.
+plan bootstrap_ospuppet (
+  TargetSpec $targets,
+  Stdlib::Absolutepath $tmp_dir = '/tmp/modules',
+) {
+  # Install Puppet agent on target(s) so that Puppet code can run
+  $targets.apply_prep
+  run_command("rm -rf ${tmp_dir}", $targets)
+
+  out::message('# Install puppet/r10k module')
+  run_command(@("CMD"/L), $targets)
+    /opt/puppetlabs/bin/puppet module install \
+    --environment production --modulepath=${tmp_dir} \
+    puppet/r10k
+    | CMD
+
+  out::message('# Configure r10k')
+  run_command(@("CMD"/L), $targets)
+    /opt/puppetlabs/bin/puppet apply \
+    --environment production --modulepath=/tmp/modules \
+    -e "class { 'r10k': remote => 'https://github.com/ndelic0/ospuppet-control-repo.git' }"
+    | CMD
+  out::message('# Run the deploy command')
+  run_command("r10k deploy environment production -pv || exit 1", $targets, '_run_as' => 'root')
+
+}
